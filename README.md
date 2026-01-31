1d7623ddf7c38a0f1939a01edefd2571c4092bf6
Biquaternions <biquaternions@serlith.net>
2026-01-31 15:05:00 -0500

    Measure time and update README.md

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 
 # Jet
 
-> [!WARNING]
-> Jet is still under development and is not in a usable state \
-> Some features are still missing
+A backend service for [Flare](https://github.com/TECHNOVE/Flare) and [flare-viewer](https://github.com/SerlithNetwork/flare-viewer).
 
-A backend service for [Flare](https://github.com/TECHNOVE/Flare) and [flare-viewer](https://github.com/SerlithNetwork/flare-viewer)
+### Compiling for production
+
+Compile the project by running
+````shell
+  gradlew build
+````
+
+Then run it using
+````shell
+  java -jar Jet.jar
+````
+
+### Supported Frontends
+Jet was designed to work on pair with [flare-viewer](https://github.com/SerlithNetwork/flare-viewer). \
+Since the Flare protocol expects a single endpoint for both submitting data and using the web-ui, it is
+highly recommended to use a reverse proxy like Nginx or Caddy to forward the requests accordingly.
+
+### Client
+Jet is compatible with any platform that provides [Flare](https://github.com/TECHNOVE/Flare)
+1. As a standalone plugin \
+   1.1. [FlarePlugin](https://github.com/TECHNOVE/FlarePlugin) \
+   1.2. [FlarePlatform](https://github.com/SerlithNetwork/FlarePlatform) (recommended)
+2. Provided by a server software \
+   2.1. [Pufferfish](https://github.com/pufferfish-gg/Pufferfish) and forks \
+   2.2. [Puffernot](https://github.com/SerlithNetwork/Puffernot)/[Pufferfork](https://github.com/Toffikk/Pufferfork) and forks (recommended)
 
 ### License
 
--- src/main/kotlin/net/serlith/jet/controller/ApiController.kt
+++ src/main/kotlin/net/serlith/jet/controller/ApiController.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import kotlin.jvm.optionals.getOrNull
+import kotlin.time.measureTimedValue
 
 @RestController
 @RequestMapping("/api")
@@ -36,14 +37,17 @@ class ApiController (
         @PathVariable key: String,
     ): ResponseEntity<FlareProfileResponse> {
 
-        val flare = this.flareRepository.findById(key).getOrNull() ?: return ResponseEntity.notFound().build()
-        val response = FlareProfileResponse(
-            raw = flare.raw,
-            dataSamples = flare.dataSamples.map { s -> s.raw },
-            timelineSamples = flare.timelineSamples.map { s -> s.raw },
-            createdAt = flare.createdAt,
-        )
         this.logger.info("Requested profile '$key' from ${request.remoteAddr}:${request.remotePort}")
+        val (response, elapsed) = measureTimedValue {
+            val flare = this.flareRepository.findById(key).getOrNull() ?: return ResponseEntity.notFound().build()
+            return@measureTimedValue FlareProfileResponse(
+                raw = flare.raw,
+                dataSamples = flare.dataSamples.map { s -> s.raw },
+                timelineSamples = flare.timelineSamples.map { s -> s.raw },
+                createdAt = flare.createdAt,
+            )
+        }
+        this.logger.info("Request profile $key took ${elapsed.inWholeMilliseconds}ms")
 
         return ResponseEntity.ok(response)
     }
