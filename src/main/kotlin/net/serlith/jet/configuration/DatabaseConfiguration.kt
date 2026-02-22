a2ac46f4cd6a970d3e32a5be6b72e96b4a3e5a91
Biquaternions <biquaternions@serlith.net>
2026-02-22 02:11:29 -0500

    Remove dead variable

--- src/main/kotlin/net/serlith/jet/configuration/DatabaseConfiguration.kt
+++ src/main/kotlin/net/serlith/jet/configuration/DatabaseConfiguration.kt
@@ -1,7 +1,6 @@
 package net.serlith.jet.configuration
 
 import io.r2dbc.spi.ConnectionFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
@@ -12,9 +11,6 @@ import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @Configuration
 class DatabaseConfiguration {
-
-    @Value($$"${jet.database.engine:H2}")
-    lateinit var engine: String
 
     @Bean
     fun connectionInitializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
