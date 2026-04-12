70ce8a51f5a5657dd2fe05e46a49dac5aeb7ca18
Biquaternions <biquaternions@serlith.net>
2026-04-12 00:46:20 -0500

    skill issue

--- src/main/kotlin/net/serlith/jet/database/repository/DataSampleRepository.kt
+++ src/main/kotlin/net/serlith/jet/database/repository/DataSampleRepository.kt
@@ -2,8 +2,10 @@ package net.serlith.jet.database.repository
 
 import net.serlith.jet.database.types.DataSample
 import org.springframework.data.r2dbc.repository.R2dbcRepository
+import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
+@Repository
 interface DataSampleRepository : R2dbcRepository<DataSample, Long> {
     fun findAllByProfile(key: String): Flux<DataSample>
 }
--- src/main/kotlin/net/serlith/jet/database/repository/TimelineSampleRepository.kt
+++ src/main/kotlin/net/serlith/jet/database/repository/TimelineSampleRepository.kt
@@ -2,8 +2,10 @@ package net.serlith.jet.database.repository
 
 import net.serlith.jet.database.types.TimelineSample
 import org.springframework.data.r2dbc.repository.R2dbcRepository
+import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
+@Repository
 interface TimelineSampleRepository : R2dbcRepository<TimelineSample, Long> {
     fun findAllByProfile(key: String): Flux<TimelineSample>
 }
