a6532d5c9fb22e218738598895a3fb08e6550379
Biquaternions <biquaternions@serlith.net>
2026-06-18 22:37:11 -0500

    Add createdAt field

--- src/main/kotlin/net/serlith/jet/types/management/FlareManagerDetails.kt
+++ src/main/kotlin/net/serlith/jet/types/management/FlareManagerDetails.kt
@@ -1,6 +1,8 @@
 package net.serlith.jet.types.management
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import net.serlith.jet.schema.tables.records.FlareManagerRecord
+import java.time.LocalDateTime
 
 abstract class FlareManagerDetails {
 
@@ -8,12 +10,16 @@ abstract class FlareManagerDetails {
 
     data class View (
         override val username: String,
+
+        @field:JsonProperty("created_at")
+        val createdAt: LocalDateTime,
     ): FlareManagerDetails() {
 
         companion object {
             fun fromRecord(record: FlareManagerRecord): View {
                 return View(
                     username = record.username,
+                    createdAt = record.createdAt,
                 )
             }
         }
